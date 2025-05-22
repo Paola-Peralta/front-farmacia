e75f06c76b9b2085f93c63d3849f337d491b5820
paolaperalta22 <paolaperalta063@gmail.com>
2025-05-21 21:56:44 -0600

    utilizando el dom para renderizar paginas

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const toggle = document.querySelector(".toggle");
 const menuDashboard = document.querySelector(".menu-dashboard");
 const iconoMenu = toggle.querySelector("i");
 const enlacesMenu = document.querySelectorAll(".enlace");
+const contenedor = document.getElementById("contenido-principal");
 
 toggle.addEventListener("click", () => {
   menuDashboard.classList.toggle("open");
@@ -14,7 +15,19 @@ toggle.addEventListener("click", () => {
 
 enlacesMenu.forEach((enlace) => {
   enlace.addEventListener("click", () => {
-    menuDashboard.classList.add("open");
-    iconoMenu.classList.replace("bx-menu", "bx-x");
+    const seccion = enlace.getAttribute("data-seccion"); // ej: "clientes"
+    fetch(`pages/${seccion}.html`)
+      .then(res => res.text())
+      .then(html => {
+        contenedor.innerHTML = html; // Muestra el contenido en el div
+      });
   });
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  fetch("pages/inicio.html")
+    .then(res => res.text())
+    .then(html => {
+      contenedor.innerHTML = html;
+    });
 });
