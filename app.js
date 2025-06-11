4ddf36f28fcdd89ee59b59e61e2b9521839cabd2
paola <paolaperalta063@gmail.com>
2025-06-10 18:00:36 -0600

    primeros estilos del menu

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-const toggle = document.querySelector(".toggle");
-const menuDashboard = document.querySelector(".menu-dashboard");
-const iconoMenu = toggle.querySelector("i");
-const enlacesMenu = document.querySelectorAll(".enlace");
-const contenedor = document.getElementById("contenido-principal");
+// const toggle = document.querySelector(".toggle");
+// const menuDashboard = document.querySelector(".menu-dashboard");
+// const iconoMenu = toggle.querySelector("i");
+// const enlacesMenu = document.querySelectorAll(".enlace");
+// const contenedor = document.getElementById("contenido-principal");
 
-toggle.addEventListener("click", () => {
-  menuDashboard.classList.toggle("open");
-  if (iconoMenu.classList.contains("bx-menu")) {
-    iconoMenu.classList.replace("bx-menu", "bx-x");
-  } else {
-    iconoMenu.classList.replace("bx-x", "bx-menu");
-  }
-});
+// toggle.addEventListener("click", () => {
+//   menuDashboard.classList.toggle("open");
+//   if (iconoMenu.classList.contains("bx-menu")) {
+//     iconoMenu.classList.replace("bx-menu", "bx-x");
+//   } else {
+//     iconoMenu.classList.replace("bx-x", "bx-menu");
+//   }
+// });
 
 // enlacesMenu.forEach((enlace) => {
 //   enlace.addEventListener("click", () => {
--- scripts/clientes.js
+++ scripts/clientes.js
@@ -1,21 +1,21 @@
 
-document.addEventListener('DOMContentLoaded', () => {
-  const modal = document.getElementById("modal");
-  const btnOpen = document.getElementById("new");
-  const btnClose = document.getElementById("closeModal");
+// document.addEventListener('DOMContentLoaded', () => {
+//   const modal = document.getElementById("modal");
+//   const btnOpen = document.getElementById("new");
+//   const btnClose = document.getElementById("closeModal");
 
-  btnOpen.addEventListener("click", () => {
-    modal.classList.remove("hidden");
-  });
+//   btnOpen.addEventListener("click", () => {
+//     modal.classList.remove("hidden");
+//   });
 
-  btnClose.addEventListener("click", () => {
-    modal.classList.add("hidden");
-  });
+//   btnClose.addEventListener("click", () => {
+//     modal.classList.add("hidden");
+//   });
 
-  window.addEventListener("click", (e) => {
-  const modal = document.getElementById("modal");
-  if (e.target === modal) {
-    modal.classList.add("hidden");
-  }
-});
-})
+//   window.addEventListener("click", (e) => {
+//   const modal = document.getElementById("modal");
+//   if (e.target === modal) {
+//     modal.classList.add("hidden");
+//   }
+// });
+// })
