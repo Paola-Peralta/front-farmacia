d4b8e0464e6978574f3ab15ea31256149f26f4c0
paola <paolaperalta063@gmail.com>
2025-06-16 20:22:17 -0600

    ads

--- app.js
+++ app.js
@@ -63,37 +63,114 @@ toggleSidebar.addEventListener('click', function () {
 })
 
 
-// Esperar a que el DOM cargue
-document.addEventListener("DOMContentLoaded", function () {
-  const links = document.querySelectorAll(".sidebar__menu a");
-  const content = document.getElementById("contentMain");
+// // Esperar a que el DOM cargue
+// document.addEventListener("DOMContentLoaded", function () {
+//   const links = document.querySelectorAll(".sidebar__menu a");
+//   const content = document.getElementById("contentMain");
 
-  links.forEach(link => {
-    link.addEventListener("click", function (e) {
-      const url = this.getAttribute("href");
+//   links.forEach(link => {
+//     link.addEventListener("click", function (e) {
+//       const url = this.getAttribute("href");
 
-      // Verifica que sea una ruta relativa válida
-      if (url && url !== "#") {
-        e.preventDefault(); // Prevenir navegación completa
+//       // Verifica que sea una ruta relativa válida
+//       if (url && url !== "#") {
+//         e.preventDefault(); // Prevenir navegación completa
 
-        fetch(url)
-          .then(response => {
-            if (!response.ok) throw new Error("Página no encontrada");
-            return response.text();
-          })
-          .then(html => {
-            // Extraer solo el contenido que te interesa
-            const parser = new DOMParser();
-            const doc = parser.parseFromString(html, "text/html");
-            const mainContent = doc.querySelector("main") || doc.body;
+//         fetch(url)
+//           .then(response => {
+//             if (!response.ok) throw new Error("Página no encontrada");
+//             return response.text();
+//           })
+//           .then(html => {
+//             // Extraer solo el contenido que te interesa
+//             const parser = new DOMParser();
+//             const doc = parser.parseFromString(html, "text/html");
+//             const mainContent = doc.querySelector("main") || doc.body;
 
-            content.innerHTML = mainContent.innerHTML;
-          })
-          .catch(err => {
-            content.innerHTML = `<p style="color:red">Error cargando contenido</p>`;
-            console.error(err);
-          });
+//             content.innerHTML = mainContent.innerHTML;
+//           })
+//           .catch(err => {
+//             content.innerHTML = `<p style="color:red">Error cargando contenido</p>`;
+//             console.error(err);
+//           });
+//       }
+//     });
+//   });
+// });
+
+document.querySelectorAll('.sidebar__dropdown a').forEach(link => {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
+      const targetId = this.dataset.target;
+
+      // Oculta todas las secciones
+      document.querySelectorAll('.main__section').forEach(section => {
+        section.style.display = 'none';
+      });
+
+      // Muestra la sección correspondiente
+      const targetSection = document.getElementById(targetId);
+      if (targetSection) {
+        targetSection.style.display = 'block';
       }
     });
   });
+
+
+  document.querySelectorAll('.main__section').forEach(section => {
+  section.style.display = 'none';
 });
+
+//******************modal cliente******************
+
+console.log("clientes.js cargado");
+
+const modal = document.querySelector('#modalAPI');
+const tbody = document.querySelector('#clientTableBody');
+
+//Elementos del modal del formulario
+
+const codigo = document.querySelector('#n-codigo');
+const nombre = document.querySelector('#n-name');
+const pApellido = document.querySelector('#n-fname');
+const sApellido = document.querySelector('#n-sname');
+const direccion = document.querySelector('#n-address');
+const telefono = document.querySelector('#n-phone');
+
+//inicializado las variables globales
+let items;
+let id;
+
+function openModal(edit = false, index = 0){
+     modal.classList.add('active');
+
+    modal.onclick = e => {
+        // if (e.target.id === 'modalAPI') {
+        //     modal.classList.remove('active');
+        // }
+        if(e.target.className.indexOf('modalAPI')!== -1)
+        {
+            modal.classList.remove('active');
+        }
+    }
+
+    if(edit){
+        codigo.value = items[index].codigo
+        nombre.value = items[index].nombre
+        pApellido.value = items[index].pApellido
+        sApellido.value = items[index].sApellido
+        direccion.value = items[index].direccion
+        telefono.value = items[index].telefono
+        id = index
+
+    }
+
+    else{
+        codigo.value = ''
+        nombre.value = ''
+        pApellido.value = ''
+        sApellido.value = ''
+        direccion.value = ''
+        telefono.value = ''
+    }
+}
