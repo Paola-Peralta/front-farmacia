9a8ca58ed8951968a53324b3a1dfe6972ae8cdca
Daniela Rosales <rosalesalemandanielasarahi@gmail.com>
2025-06-18 19:43:04 -0600

    Modales

--- app.js
+++ app.js
@@ -203,4 +203,125 @@ closeBtn.addEventListener('click', () => {
   modal.classList.remove('active');
 });
 
+//******************MODAL PROOVEDORES******************//
+
+const proveedorModal = document.querySelector('#modalProveedor');
+const proveedorTbody = document.querySelector('#ProveedorTableBody');
+const proveedorCloseBtn = proveedorModal.querySelector('#close-btn-proveedores');
+
+//Elementos del modal del formulario
+
+const pvCodigo = document.querySelector('#pv-codigo');
+const pvNombre = document.querySelector('#pv-name');
+const pvApellido = document.querySelector('#pv-fname');
+const pvSapellido = document.querySelector('#pv-sname');
+const pvCorreo = document.querySelector('#pv-correo');
+const pvDireccion = document.querySelector('#pv-address');
+const pvTelefono = document.querySelector('#pv-phone');
+
+//inicializado las variables globales
+let itemsProveedor;
+let idProveedor;
+
+function openModalProveedores(edit = false, index = 0){
+     proveedorModal.classList.add('active');
+
+    proveedorModal.onclick = e => {
+
+        if(e.target.className.indexOf('modalProveedor')!== -1)
+        {
+            proveedorModal.classList.remove('active');
+        }
+    }
+
+    if(edit){
+        pvCodigo.value = itemsProveedor[index].pvCodigo
+        pvNombre.value = itemsProveedor[index].pvNombre
+        pvApellido.value = itemsProveedor[index].pvApellido
+        pvSapellido.value = itemsProveedor[index].pvSapellido
+        pvCorreo.value = itemsProveedor[index].pvCorreo
+        pvDireccion.value = itemsProveedor[index].pvDireccion
+        pvTelefono.value = itemsProveedor[index].pvTelefono
+        idProveedor = index
+
+    }
+
+    else{
+        pvCodigo.value = ''
+        pNombre.value = ''
+        pvApellido.value = ''
+        pvSapellido.value = ''
+        pvCorreo.value = ''
+        pvDireccion.value = ''
+        pvTelefono.value = ''
+    }
+}
+
+proveedorCloseBtn.addEventListener('click', () => {
+  proveedorModal.classList.remove('active');
+});
+
+
+
+
+//******************MODAL PRODUCTOS******************//
+
+const ProductoModal = document.querySelector('#modalProductos');
+const ProductotBody = document.querySelector('#productsTableBody');
+const ProductoCloseBtn = ProductoModal.querySelector('#close-productos');
+
+//Elementos del modal del formulario
+
+const pCodigo = document.querySelector('#p-codigo');
+const pNombre = document.querySelector('#p-name');
+const pCantidad = document.querySelector('#cantidad');
+const pFechaNacimiento = document.querySelector('#fechaVencimiento');
+const pPrecio = document.querySelector('#precio');
+const pCategoria = document.querySelector('#categoria');
+const pUnidad = document.querySelector('#unidadMedida');
+const pPresentacion = document.querySelector('#presentacion');
+
+//inicializado las variables globales
+let itemsProductos;
+let idProductos;
+
+function openModalProductos(edit = false, index = 0){
+     ProductoModal.classList.add('active');
+
+    ProductoModal.onclick = e => {
+        if(e.target.className.indexOf('modalProductos')!== -1)
+        {
+            ProductoModal.classList.remove('active');
+        }
+    }
+
+    if(edit){
+        pCodigo.value = itemsProductos[index].pCodigo
+        pNombre.value = itemsProductos[index].pNombre
+        pCantidad.value = itemsProductos[index].pCantidad
+        pFechaNacimiento.value = itemsProductos[index].pFechaNacimiento
+        pPrecio.value = itemsProductos[index].pPrecio
+        pCategoria.value = itemsProductos[index].pCategoria
+        pUnidad.value = itemsProductos[index].pUnidad
+        pPresentacion.value = itemsProductos[index].pPresentacion
+        idProductos = index
+
+    }
+
+    else{
+        pCodigo.value = ''
+        pNombre.value = ''
+        pCantidad.value = ''
+        pFechaNacimiento.value = ''
+        pPrecio.value = ''
+        pCategoria.value = ''
+        pUnidad.value = ''
+        pPresentacion.value = ''
+    }
+}
+
+ProductoCloseBtn.addEventListener('click', () => {
+  ProductoModal.classList.remove('active');
+});
+
 
